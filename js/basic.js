79ec4d9ec0fc0c8def85c2d6deea5efc5c46ecdc
Aapzu <aapeli.haanpuu@unifina.com>
2016-06-20 14:49:25 +0300

    and again musch forward, technically

--- js/basic.js
+++ js/basic.js
@@ -2,6 +2,8 @@ $(function(){
     $(".img-div").each(function() {
         $(this).css("background-image", 'url(' + $(this).data("background") + ')')
     })
+
+    // Scrolling effect
     $(".navbar a[href^='#']").each(function(i, el) {
         $(el).click(function(e) {
             e.preventDefault()
@@ -10,5 +12,19 @@ $(function(){
             }, 500)
         })
     })
+
+    // Auto Hiding Navbar
     $("nav.navbar").autoHidingNavbar()
+
+    // Carousel
+    $("#myCarousel .fullscreen-btn").click(function() {
+        $("#myCarousel").addClass("open")
+        $("body").addClass("modal-open")
+        $("nav.navbar").hide()
+    })
+    $("#myCarousel .carousel-backdrop").click(function() {
+        $("#myCarousel").removeClass("open")
+        $("body").removeClass("modal-open")
+        $("nav.navbar").show()
+    })
 })
