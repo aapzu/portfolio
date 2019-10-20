48952d508ff36a40bde9c5a43a8bbb2478f01474
Aapeli Haanpuu <aapeli.haanpuu@gofore.com>
2019-10-20 12:15:12 +0300

    Edit scripts

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
-ssh $IP <<EOF
+ssh ec2-user@$IP <<EOF
   cd ~/portfolio
   git reset --hard HEAD
   git pull
   echo "Running npm install"
   npm install
   npm run build
-  pm2 start ecosystem.config.js
+  pm2 start pm2-ecosystem.json
   echo "Latest version of portfolio running."
 EOF
