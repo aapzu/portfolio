6f6f37950e6f99ca90e3ef46b774bcd77ce58493
Aapeli Haanpuu <aapzu@iki.fi>
2022-07-04 22:33:35 +0300

    Update svelte-kit

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-  import cn from 'classnames'
-  export let href
-  let clazz: string
-  export let secondary: boolean
-  export { clazz as class }
+	import cn from 'classnames';
+	export let href;
+	let clazz: string;
+	export let secondary: boolean;
+	export { clazz as class };
 </script>
 
 <a {href} class={cn(clazz, { secondary })}>
-  <slot />
+	<slot />
 </a>
 
 <style lang="scss">
-  a {
-    color: var(--primary);
-    text-decoration: none;
-    display: inline-block;
-    position: relative;
+	a {
+		color: var(--primary);
+		text-decoration: none;
+		display: inline-block;
+		position: relative;
 
-    &.secondary:not(:hover) {
-      color: var(--secondary);
-    }
+		&.secondary:not(:hover) {
+			color: var(--secondary);
+		}
 
-    &::before {
-      content: '';
-      position: absolute;
-      height: 100%;
-      width: 0;
-      border-bottom: 2px solid var(--primary);
-      transition: width 0.3s ease, left 0s linear;
-      right: 0;
-    }
+		&::before {
+			content: '';
+			position: absolute;
+			height: 100%;
+			width: 0;
+			border-bottom: 2px solid var(--primary);
+			transition: width 0.3s ease, left 0s linear;
+			right: 0;
+		}
 
-    &:hover::before {
-      width: 100%;
-      left: 0;
-      transition-delay: 0s, 0.3s;
-    }
-  }
+		&:hover::before {
+			width: 100%;
+			left: 0;
+			transition-delay: 0s, 0.3s;
+		}
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,20 @@
-import preprocess from 'svelte-preprocess'
-import vercel from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    adapter: vercel()
-  }
-}
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-export default config
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
+};
+
+export default config;
